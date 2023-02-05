06705a2babcdca1e829a4f98c1cb83693c38a6df
Yohan Moren <yohan.moren@gmail.com>
2023-02-05 21:43:27 +0100

    Need corrections

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -1,6 +1,8 @@
 const User = require("../models/user.model")
+const Missions = require("../models/missions.model")
+const Jobs = require("../models/jobs.model")
 
-exports.viewUser = (req, res) => {
+exports.getUser = (req, res) => {
   User.findById(req.userToken.id)
     .then((user) => {
       if (!user) {
@@ -34,5 +36,73 @@ exports.updateUser = (req, res) => {
   .catch((err) => res.status(400).send(err));
   };
 
+  exports.getMissions = (req, res) => {
+    Missions.findById(req.userToken.id)
+      .then((user) => {
+        if (!user) {
+          return res.status(404).send({
+            message: "user not found",
+          });
+        }
+        res.send(user);
+      })
+      .catch((err) => res.status(400).send(err));
+  };
+  
+  exports.deleteMissions = (req, res) => {
+    Missions.findByIdAndDelete(req.params.id).then(user => {
+      res.send(user)
+    }).catch(err=>res.status(400).send(err))
+  }
+  
+  exports.updateMissions = (req, res) => {
+    Missions.findByIdAndUpdate(req.params.id, req.body)
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send({
+          message: "user not found",
+        });
+      }
+      User.findById(user._id).then((userupdated) => {
+        res.send(userupdated);
+      });
+    })
+    .catch((err) => res.status(400).send(err));
+    };
+
+    exports.getJobs = (req, res) => {
+      Jobs.findById(req.userToken.id)
+        .then((user) => {
+          if (!user) {
+            return res.status(404).send({
+              message: "user not found",
+            });
+          }
+          res.send(user);
+        })
+        .catch((err) => res.status(400).send(err));
+    };
+    
+    exports.deleteJobs = (req, res) => {
+      Jobs.findByIdAndDelete(req.params.id).then(user => {
+        res.send(user)
+      }).catch(err=>res.status(400).send(err))
+    }
+    
+    exports.updateJobs = (req, res) => {
+      Jobs.findByIdAndUpdate(req.params.id, req.body)
+      .then((user) => {
+        if (!user) {
+          return res.status(404).send({
+            message: "user not found",
+          });
+        }
+        User.findById(user._id).then((userupdated) => {
+          res.send(userupdated);
+        });
+      })
+      .catch((err) => res.status(400).send(err));
+      };
+    
 
 
--- src/routes/admin.route.js
+++ src/routes/admin.route.js
@@ -4,9 +4,15 @@ const adminController = require('../controllers/admin.controller')
 const verifyToken = require('../middlewares/verifyToken');
 const verifyIsAdmin = require('../middlewares/verifyIsAdmin')
 
-router.get('/getUser/:id', verifyToken, verifyIsAdmin, adminController.viewUser);
+router.get('/getUser/:id', verifyToken, verifyIsAdmin, adminController.getUser);
 router.delete('/removeUser/:id', verifyToken, verifyIsAdmin, adminController.deleteUser);
 router.post('/editUser/:id', verifyToken, verifyIsAdmin, adminController.updateUser);
+router.get('/getMissions/:id', verifyToken, verifyIsAdmin, adminController.getMissions);
+router.delete('/removeMissions/:id', verifyToken, verifyIsAdmin, adminController.deleteMissions);
+router.post('/editMissions/:id', verifyToken, verifyIsAdmin, adminController.updateMissions);
+router.get('/getJobs/:id', verifyToken, verifyIsAdmin, adminController.getJobs);
+router.delete('/removeJobs/:id', verifyToken, verifyIsAdmin, adminController.deleteJobs);
+router.post('/editJobs/:id', verifyToken, verifyIsAdmin, adminController.updateJobs);
 // router.post('/login', checkEmail, checkPassword, validation, authController.login);
 
 module.exports = router;
